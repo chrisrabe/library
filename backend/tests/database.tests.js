55f123df07ecd0522251fce71c9c777e5588f849
Chris Rabe <chris.rabe14@gmail.com>
2021-01-22 23:03:59 +1000

    created tests for book

--- backend/tests/database.tests.js
+++ backend/tests/database.tests.js
@@ -1,11 +1,13 @@
 // Before starting the test,
 // please ensure to run the start-dbs.sh script
+// or a mongodb docker container is running locally
 
 const mongoose = require('mongoose');
 const expect = require('chai').expect;
 const { TEST_DB_URL } = require('./constants');
 
 const AuthorModel = require('../model/author.model');
+const BookModel = require('../model/book.model');
 
 describe('Database Tests', () => {
   let authorId = undefined;
@@ -88,6 +90,71 @@ describe('Database Tests', () => {
       );
       expect(author.first_name).to.be.equal('Brian');
       expect(author.last_name).to.be.equal('Rabe');
+    });
+  });
+
+  describe('Book database tests', () => {
+    let bookId = undefined;
+
+    it('should successfully create a book', async () => {
+      const mockData = {
+        name: 'Random book',
+        isbn: 'TEST_ISBN',
+        author: authorId,
+      };
+      const doc = await BookModel.createDoc(undefined, mockData);
+      expect(doc.name).to.equal('Random book');
+      expect(doc.isbn).to.equal('TEST_ISBN');
+      expect(doc.author).to.equal(authorId);
+      bookId = doc._id;
+    });
+
+    it('should have one doc in the book list', async () => {
+      const query = {};
+      const list = await BookModel.findDoc(undefined, query);
+      expect(list.length).to.be.equal(1);
+      const item = list[0];
+      expect(item).to.not.equal(undefined);
+      if (item) {
+        expect(item.name).to.equal('Random book');
+        expect(item.isbn).to.equal('TEST_ISBN');
+        expect(item.author.toString()).to.equal(authorId.toString());
+      }
+    });
+
+    it('should display author details if retrieving book by id', async () => {
+      const options = {};
+      const fieldsToPopulate = ['author'];
+      const doc = await BookModel.findByIdDoc(
+        undefined,
+        bookId,
+        options,
+        fieldsToPopulate,
+      );
+      expect(doc).to.not.equal(undefined);
+      if (doc) {
+        expect(doc.name).to.equal('Random book');
+        expect(doc.isbn).to.equal('TEST_ISBN');
+        expect(doc.author.toString()).to.not.equal(authorId.toString());
+        expect(doc.author.first_name).to.be.equal('Brian');
+        expect(doc.author.last_name).to.be.equal('Rabe');
+      }
+    });
+
+    it('should be able to update book details', async () => {
+      const update = {
+        $set: {
+          name: 'To Kill a Mocking Bird',
+        },
+      };
+      const doc = await BookModel.findByIdAndUpdateDoc(
+        undefined,
+        bookId,
+        update,
+      );
+      expect(doc.name).to.equal('To Kill a Mocking Bird');
+      expect(doc.isbn).to.equal('TEST_ISBN');
+      expect(doc.author.toString()).to.equal(authorId.toString());
     });
   });
 
